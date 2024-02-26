0d29a5307a3c15edca0a556b394c54ab92f0b8a6
akash rawat <akash.rawat@enine.school>
2024-02-26 11:51:07 +0530

    Completed

--- assets.py
+++ assets.py
@@ -5,11 +5,10 @@ from datetime import date
 import datetime
 import random
 import csv
-from faker import Faker
 
 
 def create_csv(data, filename, fieldnames):
-    """ 
+    """
     creating csv file
     """
     with open(filename, "w", encoding="utf-8") as f:
@@ -23,15 +22,11 @@ def create_assets(p):
     """
     generating random data for assets
     """
-    fake = Faker()
     today_date = date.today()
-    x = datetime.date(2022, 1, 1)
-    asset_id = 1
     assets = []
-    for _ in range(p):
-        random_date = fake.date_between(x, today_date)
+    for asset_id in range(1, p + 1):
+        random_date = today_date - datetime.timedelta(days=random.randint(100, 500))
         assets.append({"id": asset_id, "purchase_date": random_date})
-        asset_id += 1
 
     fieldnames = ["id", "purchase_date"]
     create_csv(assets, "assets.csv", fieldnames)
@@ -43,31 +38,39 @@ def create_rentals(assets, q):
     """
     generating random data for rental
     """
-    rental_id = 1
     rentals = []
+    assets_copy = assets.copy()
     today_date = date.today()
-    fake = Faker()
-    for _ in range(q):
-        asset = random.choice(assets)
+    for rental_id in range(1, q + 1):
+        if not assets_copy:
+            print("No assets to rent")
+            return None
+        asset = random.choice(assets_copy)
         asset_date = asset["purchase_date"] + datetime.timedelta(days=1)
-        is_asset = []
-        for data in rentals:
-            if data["asset_id"] == asset["id"]:
-                is_asset.append(data["end_date"])
-        if is_asset:
-            asset_date = max(is_asset) + datetime.timedelta(days=1)
-        asset_id = asset["id"]
-        start_date = fake.date_between(asset_date, today_date)
-        end_date = fake.date_between(start_date, today_date)
+        end_dates = [
+            data["end_date"] for data in rentals if data["asset_id"] == asset["id"]
+        ]
+        if end_dates:
+            asset_date = max(end_dates) + datetime.timedelta(days=1)
+
+        start_date = (
+            date.today()
+            if asset_date == today_date
+            else date.today()
+            - datetime.timedelta(days=random.randint(1, (today_date - asset_date).days))
+        )
+        end_date = start_date + datetime.timedelta(days=random.randint(10, 100))
+        if end_date >= today_date:
+            assets_copy.remove(asset)
+
         rentals.append(
             {
                 "id": rental_id,
-                "asset_id": asset_id,
+                "asset_id": asset["id"],
                 "start_date": start_date,
                 "end_date": end_date,
             }
         )
-        rental_id += 1
 
     fieldnames = ["id", "asset_id", "start_date", "end_date"]
     create_csv(rentals, "rental.csv", fieldnames)
